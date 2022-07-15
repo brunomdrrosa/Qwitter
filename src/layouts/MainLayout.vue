df74a898be4a6f87cb3832b0ca5920fb2df782c4
brunomdr <brunomdr46@gmail.com>
2022-07-15 19:07:24 -0300

    Barra de navegação da esquerda

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,8 +8,25 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
-      <!-- drawer content -->
+    <q-drawer show-if-above v-model="left" side="left" bordered :width="283">
+      <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
+
+      <q-list>
+        <q-item to="/" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="home" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Página Inicial</q-item-section>
+        </q-item>
+        <q-item to="/sobre" clickable v-ripple>
+          <q-item-section avatar>
+            <q-icon name="help" size="md" />
+          </q-item-section>
+
+          <q-item-section class="text-h6 text-weight-bold">Sobre</q-item-section>
+        </q-item>
+      </q-list>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
