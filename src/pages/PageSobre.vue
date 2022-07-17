cf9c8a9c730001635bdc98ce212536e4f7208831
brunomdr <brunomdr46@gmail.com>
2022-07-17 14:42:21 -0300

    Página sobre

--- src/pages/PageSobre.vue
+++ src/pages/PageSobre.vue
@@ -1,13 +1,37 @@
 <template>
-  <q-page>
-    <h5>Sobre</h5>
+  <q-page class="q-pa-lg">
+    <h4 class="q-mt-none q-mb-md text-weight-bold">Sobre Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac
+        commodo est. Donec fringilla consectetur leo, et eleifend sem hendrerit
+        vel. Vivamus rhoncus lobortis malesuada. Aliquam bibendum, nisl a
+        ultrices hendrerit, ipsum dui sagittis libero, non rutrum justo metus
+        non neque. Vivamus accumsan, lectus vel mollis tristique, libero nibh
+        dictum lectus, sed tincidunt risus felis facilisis magna. Nam auctor
+        augue at ligula fermentum tempor. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Fusce augue enim,
+        varius nec cursus eu, dapibus ut nulla.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac
+        commodo est. Donec fringilla consectetur leo, et eleifend sem hendrerit
+        vel. Vivamus rhoncus lobortis malesuada. Aliquam bibendum, nisl a
+        ultrices hendrerit, ipsum dui sagittis libero, non rutrum justo metus
+        non neque. Vivamus accumsan, lectus vel mollis tristique, libero nibh
+        dictum lectus, sed tincidunt risus felis facilisis magna. Nam auctor
+        augue at ligula fermentum tempor. Orci varius natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus. Fusce augue enim,
+        varius nec cursus eu, dapibus ut nulla.
+      </p>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'PageSobre'
-})
+  name: "PageSobre",
+});
 </script>
