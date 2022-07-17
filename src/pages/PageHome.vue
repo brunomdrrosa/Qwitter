8a6e960605e1de7ea0c62317919fe973c01f3098
brunomdr <brunomdr46@gmail.com>
2022-07-17 15:32:12 -0300

    Design da lista de tweets

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -33,6 +33,37 @@
       </div>
     </div>
     <q-separator class="divider" size="10px" color="grey-2" />
+    <q-list>
+      <q-item class="q-py-md">
+        <q-item-section avatar top>
+          <q-avatar size="xl">
+            <img
+              src="https://pbs.twimg.com/profile_images/1391831038511521794/izFarcRT_400x400.jpg"
+            />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label class="text-subtitle1"
+            ><strong>Bruno Machado</strong
+            ><span class="text-grey-7">@brunomdr02</span></q-item-label
+          >
+          <q-item-label class="qweet-content text-body1">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac
+            commodo est. Donec fringilla consectetur leo, et eleifend sem
+            hendrerit vel.
+          </q-item-label>
+          <div class="qweet-icons row justify-between q-mt-sm">
+            <q-btn color="grey" icon="far fa-comment" size="sm" flat round></q-btn>
+            <q-btn color="grey" icon="fas fa-retweet" size="sm" flat round></q-btn>
+            <q-btn color="grey" icon="far fa-heart" size="sm" flat round></q-btn>
+            <q-btn color="grey" icon="fas fa-trash" size="sm" flat round></q-btn>
+          </div>
+        </q-item-section>
+
+        <q-item-section side top> 1 min ago </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
@@ -58,4 +89,8 @@ export default defineComponent({
   border-top: 1px solid
   border-bottom: 1px solid
   border-color: $grey-4
+.qweet-content
+  white-space: pre-line
+.qweet-icons
+  margin-left: -5px
 </style>
